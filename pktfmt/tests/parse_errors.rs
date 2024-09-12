f790566cd78859e29766465911bb2900fbe0db99
duanjp8617 <duanjp8617@gmail.com>
2024-09-12 20:11:38 +0800

    update field error 7

--- pktfmt/tests/parse_errors.rs
+++ pktfmt/tests/parse_errors.rs
@@ -107,7 +107,7 @@ macro_rules! test_parse_eror_list {
 }
 
 test_parse_eror_list!(
-    [field_error_1, field_error_2, field_error_3, field_error_4, field_error_5, field_error_6,
+    [field_error_1, field_error_2, field_error_3, field_error_4, field_error_5, field_error_6, field_error_7,
      header_error_1, header_error_2, header_error_3, header_error_4, 
      length_error_1, length_error_2, length_error_3, length_error_5, length_error_6,
      length_error_7, length_error_8, length_error_9, length_error_10, length_error_11, length_error_12, length_error_13],
@@ -116,12 +116,5 @@ test_parse_eror_list!(
 
 #[test]
 fn fuck() {
-    // dump_parse_error!("length_error_4.pktfmt", parser::PacketParser);
-    let s1 = "0xabc";
-    let s2 = "0xAbc";
-
-    let n1 = u64::from_str_radix(&s1[2..], 16).unwrap();
-    let n2 = u64::from_str_radix(&s2[2..], 16).unwrap();
-
-    println!("{n1}, {n2}");
+    dump_parse_error!("field_error_7.pktfmt", parser::PacketParser);
 }
