0dccddb05222ded5125e0a00373e80f894c36d3f
duanjp8617 <duanjp8617@gmail.com>
2024-09-14 11:24:15 +0800

    length field generation considers fixed default value

--- pktfmt/src/codegen/length.rs
+++ pktfmt/src/codegen/length.rs
@@ -1,6 +1,6 @@
 use std::io::Write;
 
-use crate::ast::{max_value, BitPos, BuiltinTypes, Field, UsableAlgExpr};
+use crate::ast::{max_value, BitPos, BuiltinTypes, DefaultVal, Field, UsableAlgExpr};
 
 use super::{FieldGetMethod, FieldSetMethod, HeadTailWriter};
 
@@ -103,19 +103,28 @@ impl<'a> LengthSetMethod<'a> {
         // Next, we calculate a series of guard conditions for the header set
         // method.
         let mut guards = Vec::new();
-        // we use a closure to check whether the max value of the input arg type is the
-        // same as the max value of the length field. If these two are not the same, we
-        // add a guard.
-        let need_max_length_guard = |max_length: u64| match &arg_type {
-            BuiltinTypes::U8 => max_length < u8::MAX as u64,
-            BuiltinTypes::U16 => max_length < u16::MAX as u64,
-            BuiltinTypes::U32 => max_length < u32::MAX as u64,
-            BuiltinTypes::U64 => max_length < u64::MAX as u64,
-            _ => panic!(),
-        };
-        let max_length = self.expr.exec(max_value(self.field.bit).unwrap()).unwrap();
-        if need_max_length_guard(max_length) {
-            guards.push(format!("{write_value}<={max_length}"));
+        if self.field.default_fix {
+            let default_val = match self.field.default {
+                DefaultVal::Num(n) => n,
+                _ => panic!(),
+            };
+            let fixed_length = self.expr.exec(default_val).unwrap();
+            guards.push(format!("{write_value}=={fixed_length}"));
+        } else {
+            // we use a closure to check whether the max value of the input arg type is the
+            // same as the max value of the length field. If these two are not the same, we
+            // add a guard.
+            let need_max_length_guard = |max_length: u64| match &arg_type {
+                BuiltinTypes::U8 => max_length < u8::MAX as u64,
+                BuiltinTypes::U16 => max_length < u16::MAX as u64,
+                BuiltinTypes::U32 => max_length < u32::MAX as u64,
+                BuiltinTypes::U64 => max_length < u64::MAX as u64,
+                _ => panic!(),
+            };
+            let max_length = self.expr.exec(max_value(self.field.bit).unwrap()).unwrap();
+            if need_max_length_guard(max_length) {
+                guards.push(format!("{write_value}<={max_length}"));
+            }
         }
 
         let guard_str = self.expr.reverse_exec_guard(write_value);
