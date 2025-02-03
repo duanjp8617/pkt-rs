117c2bfb9a1f2529fa0c131c335fe80d48b66354
duanjp8617 <duanjp8617@gmail.com>
2025-02-03 20:58:51 +0800

    the pktfmt command works

--- pktfmt/bin/pktfmt.rs
+++ pktfmt/bin/pktfmt.rs
@@ -1,10 +1,73 @@
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
+use std::path::PathBuf;
 
 use pktfmt::{ast, codegen, file_text, parser, token, utils};
 
-fn driver(file_text: &file_text::FileText, output_file: &str) -> Result<(), utils::Error> {
+// A helper that will write an "error" line to the output channel.
+fn preceed_with_error(output: &mut dyn Write) -> &mut dyn Write {
+    writeln!(output, "error").unwrap();
+    output
+}
+
+fn handle_args(args: &Vec<String>) -> Result<(PathBuf, PathBuf), String> {
+    let mut input_path = None;
+    let mut output_path = None;
+
+    let help = r#"Usage: pktfmt <*.pktfmt> [options]
+Options:
+  -o <file>   Generate the output in <file>.
+  -h          Display help information.
+"#;
+
+    let mut i = 1;
+    while i < args.len() {
+        if (&args[i]).ends_with(".pktfmt") && args[i].len() > ".pktfmt".len() {
+            // Find a ".pktfmt" suffix, which should be an input file
+            input_path = Some(
+                fs::canonicalize(&PathBuf::from(&args[i]))
+                    .map_err(|_| format!("invalid file path: {}", &args[i]))?,
+            );
+            i += 1;
+        } else if &args[i] == "-o" && (i + 1 < args.len()) {
+            output_path = Some(
+                fs::canonicalize(&PathBuf::from(&args[i + 1]))
+                    .map_err(|_| format!("invalid file path: {}", &args[i + 1]))?,
+            );
+            i += 2;
+        } else if &args[i] == "-h" {
+            print!("{help}");
+            std::process::exit(0);
+        } else {
+            i += 1
+        }
+    }
+
+    match (input_path, output_path) {
+        (Some(i), Some(o)) => Ok((i, o)),
+        (Some(i), None) => {
+            let fname = i
+                .file_name()
+                .and_then(|s| s.to_str())
+                .ok_or(format!("invalid file name"))?;
+            let end_idx = fname.rfind(".pktfmt").ok_or(format!("invalid file name"))?;
+            if end_idx > 0 {
+                let o = std::env::current_dir()
+                    .unwrap()
+                    .join(format!("{}.rs", &fname[..end_idx]));
+                Ok((i, o))
+            } else {
+                Err(format!("invalid file name"))
+            }
+        }
+        _ => Err(format!("missing input file")),
+    }
+}
+
+// The driver function that runs the code generation pipeline.
+fn driver(file_text: &file_text::FileText, output_file: &PathBuf) -> Result<(), utils::Error> {
+    // Parse for the top level ast.
     let tokenizer = token::Tokenizer::new(file_text.text());
     let (start_code, parsed_items, end_code_opt) = parser::TopLevelParser::new()
         .parse(
@@ -21,7 +84,7 @@ fn driver(file_text: &file_text::FileText, output_file: &str) -> Result<(), util
 
     // Prepare the output channel.
     let mut output_f = File::create(output_file)
-        .map_err(|err| utils::Error::ErrStr(format!("{output_file}: {err}")))?;
+        .map_err(|err| utils::Error::ErrStr(format!("{}: {err}", output_file.to_str().unwrap())))?;
 
     // Do the code generation.
     write!(&mut output_f, "{start_code}").unwrap();
@@ -53,29 +116,30 @@ fn driver(file_text: &file_text::FileText, output_file: &str) -> Result<(), util
 }
 
 fn main() {
-    let mut args = env::args();
-    if args.len() != 2 {
-        eprintln!(
-            "Currently we only accept a single argument, which is the name of the input file."
-        );
-        std::process::exit(1);
-    }
-    args.next();
+    let args: Vec<String> = env::args().into_iter().collect();
+    let mut stderr = std::io::stderr();
 
-    let input_file = args.next().unwrap();
+    // Handle the input args
+    let (input_file, output_file) = match handle_args(&args) {
+        Ok(res) => res,
+        Err(err_str) => {
+            writeln!(preceed_with_error(&mut stderr), "{err_str}").unwrap();
+            std::process::exit(1);
+        }
+    };
 
     // Prepare the file text.
     let file_text = match file_text::FileText::new(input_file) {
         Ok(file_text) => file_text,
         Err(e) => {
-            eprintln!("{e}");
-            std::process::exit(1)
+            writeln!(preceed_with_error(&mut stderr), "{e}").unwrap();
+            std::process::exit(1);
         }
     };
 
-    match driver(&file_text, "./tmp.rs") {
+    // Run the code generation driver.
+    match driver(&file_text, &output_file) {
         Err(err) => {
-            let mut stderr = std::io::stderr();
             utils::render_error(&file_text, err, &mut stderr);
             std::process::exit(1)
         }
